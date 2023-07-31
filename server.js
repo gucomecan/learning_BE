8f649d5c62f36eefe7ee484a01423657d76b39e5
Velizar A. Kitanov <velizar.kitanov@musala.com>
2023-07-31 08:16:28 +0300

    feat(server): add base POST and logger middleware

--- server.js
+++ server.js
@@ -3,11 +3,29 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
-const resource_friends = {
+let resource_friends = {
   0: 'Issac Newton',
   1: 'Le Friend of mine',
   2: 'Le Test Friend'
 }
+
+const logger = (req, res, next) => {
+  const startTime = Date.now()
+  next()
+  const endTime = Date.now()
+  console.log(`---${req.method}: ${req.url} -> ${endTime - startTime}ms`)
+}
+
+app.use(logger)
+app.use(express.json())
+
+app.post('/friends', (req, res) => {
+  if (!req.body.name) return res.status(400).json({ error: 'Bad input for creating a friend' })
+
+  const id = Object.keys(resource_friends).length
+  resource_friends[id] = req.body.name
+  res.status(200).json(resource_friends[id])
+})
 
 app.get('/friends', (req, res) => {
   res.json(resource_friends)
