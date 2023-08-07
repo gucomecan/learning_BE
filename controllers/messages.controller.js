d7eac2bb930fdce2b48c05c04fae46238224e2a4
Velizar A. Kitanov <velizar.kitanov@musala.com>
2023-08-07 16:53:24 +0300

    feat(views): add hbs and some views to test render functionality

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require('path')
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
+  res.render('messages', {
+    title: 'Messages',
+    friend: 'Le Feropino'
+  })
 }
 
 function postMessages(req, res) {
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const messagesRouter = require('./routes/messages.router')
 
 const PORT = 3000
 const app = express()
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
 
 const logger = (req, res, next) => {
   const startTime = Date.now()
@@ -14,9 +16,16 @@ const logger = (req, res, next) => {
   console.log(`---${req.method}: ${req.baseUrl}${req.url} -> ${delta}ms`)
 }
 
+
 app.use('/site', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(logger)
+app.get('/', (req, res) => {
+  res.render('where', {
+    title: 'Test pages',
+    country: 'Le France'
+  })
+})
 app.use('/friends', friendsRouter)
 app.use('/messages', messagesRouter)
 
