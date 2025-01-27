a14f31c340a8f78b65882d263061ceaf2763c01d
gucomecan <vkitanov92@gmail.com>
2025-01-27 08:49:57 +0200

    feat: extract replaceTemplate as module

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const http = require('http')
 const url = require('url')
+const replaceTemplate = require('./modules/replateTemplate')
 
 const dataOfProducts = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(dataOfProducts)
@@ -16,22 +17,6 @@ const page404 = (res) => {
   return
 }
 
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
-  output = output.replace(/{%IMAGE%}/g, product.image)
-  output = output.replace(/{%FROM%}/g, product.from)
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-  output = output.replace(/{%QUANTITY%}/g, product.quantity)
-  output = output.replace(/{%PRICE%}/g, product.price)
-  output = output.replace(/{%DESCRIPTION%}/g, product.description)
-  output = output.replace(/{%ID%}/g, product.id)
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-  }
-
-  return output
-}
 
 // ---------- server -------------
 const server = http.createServer((req, res) => {
